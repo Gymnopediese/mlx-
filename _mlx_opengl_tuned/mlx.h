1edb5d1f1aee02990cdb814f8530e0ea0e4031bf
gymnopediese <kalityoflife@gmail.com>
2023-09-28 17:26:27 +0200

    m

--- _mlx_opengl_tuned/mlx.h
+++ _mlx_opengl_tuned/mlx.h
@@ -119,10 +119,10 @@ int	mlx_loop (void *mlx_ptr);
 
 int	mlx_string_put(void *mlx_ptr, void *win_ptr, int x, int y, int color,
 		       char *string);
-void	*mlx_xpm_to_image(void *mlx_ptr, char **xpm_data,
-			  int *width, int *height);
-void	*mlx_xpm_file_to_image(void *mlx_ptr, char *filename,
-			       int *width, int *height);
+// void	*mlx_xpm_to_image(void *mlx_ptr, char **xpm_data,
+			//   int *width, int *height);
+// void	*mlx_xpm_file_to_image(void *mlx_ptr, char *filename,
+			    //    int *width, int *height);
 void    *mlx_png_file_to_image(void *mlx_ptr, char *file, int *width, int *height);
 
 int	mlx_destroy_window(void *mlx_ptr, void *win_ptr);
@@ -145,7 +145,7 @@ int     mlx_mouse_get_pos(void *win_ptr, int *x, int *y);
 
 int	mlx_do_key_autorepeatoff(void *mlx_ptr);
 int	mlx_do_key_autorepeaton(void *mlx_ptr);
-int	mlx_do_sync(void *mlx_ptr);
+// int	mlx_do_sync(void *mlx_ptr);
 
 
 // Julio :
